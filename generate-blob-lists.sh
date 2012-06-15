5ff7a5cd8118944c92769f12e8db030f97bbe924
Jean-Baptiste Queru <jbq@google.com>
2012-06-15 09:30:00 -0700

    stop auto-building unsupported configs
    Change-Id: If549471a96efe55e0da1ae76fe4a608dbd442709

--- generate-blob-lists.sh
+++ generate-blob-lists.sh
@@ -42,7 +42,7 @@ then
 fi
 shift
 
-DEVICES="crespo crespo4g stingray wingray panda toro torospr maguro grouper manta"
+DEVICES="crespo wingray panda maguro grouper manta"
 export LC_ALL=C
 
 repo sync -j32 -n
