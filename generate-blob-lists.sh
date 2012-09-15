37fd149ef28abca79291b000abbd422e179926f8
Jean-Baptiste Queru <jbq@google.com>
2012-09-15 09:41:10 -0700

    Add toroplus to list of target devices
    Bug: 6940653
    Change-Id: I58c4cdbb88f4aadcd7730d5d89a193cad8185934

--- generate-blob-lists.sh
+++ generate-blob-lists.sh
@@ -42,7 +42,7 @@ then
 fi
 shift
 
-DEVICES="crespo crespo4g wingray panda maguro toro grouper"
+DEVICES="crespo crespo4g wingray panda maguro toro toroplus grouper"
 export LC_ALL=C
 
 repo sync -j32 -n
