52dc95d00b936f5ae9a999787a46dcc0be55d3c1
Bill Yi <byi@google.com>
2016-09-09 11:22:13 -0700

    Switch tar to zip for factory images
    Change-Id: I7a077d5ef540784efbee654b7e564a9dab2626b9

--- generate-factory-images-common.sh
+++ generate-factory-images-common.sh
@@ -293,8 +293,8 @@ fi
 chmod a+x tmp/$PRODUCT-$VERSION/flash-base.sh
 
 # Create the distributable package
-(cd tmp ; tar zcvf ../$PRODUCT-$VERSION-factory.tgz $PRODUCT-$VERSION)
-mv $PRODUCT-$VERSION-factory.tgz $PRODUCT-$VERSION-factory-$(sha1sum < $PRODUCT-$VERSION-factory.tgz | cut -b -8).tgz
+(cd tmp ; zip -r ../$PRODUCT-$VERSION-factory.zip $PRODUCT-$VERSION)
+mv $PRODUCT-$VERSION-factory.zip $PRODUCT-$VERSION-factory-$(sha256sum < $PRODUCT-$VERSION-factory.zip | cut -b -8).zip
 
 # Clean up
 rm -rf tmp
