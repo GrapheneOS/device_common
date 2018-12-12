31e140cc6e1f9328230ec95dde4c0c3986095dba
Daniel Micay <danielmicay@gmail.com>
2018-12-12 01:10:55 -0500

    include AVB custom key with factory images [1/2]

--- clear-factory-images-variables.sh
+++ clear-factory-images-variables.sh
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+unset AVB_PKMD
 unset BOOTLOADER
 unset BOOTLOADERFILE
 unset BOOTLOADERSRC
