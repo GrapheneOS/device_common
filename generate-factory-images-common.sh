52b54aea5b84e042547f431ee114088db863b3f2
Chirayu Desai <chirayudesai1@gmail.com>
2020-06-03 03:59:34 +0530

    Reboot to bootloader after installing update
    * Can't lock bootloader from fastbootd,
      reboot to bootloader to allow that.
    * Doesn't hurt on other devices, keeps the script simple.

--- generate-factory-images-common.sh
+++ generate-factory-images-common.sh
@@ -178,6 +178,8 @@ EOF
 fi
 cat >> tmp/$PRODUCT-$VERSION/flash-all.sh << EOF
 fastboot -w --skip-reboot update image-$PRODUCT-$VERSION.zip
+fastboot reboot-bootloader
+sleep $SLEEPDURATION
 EOF
 chmod a+x tmp/$PRODUCT-$VERSION/flash-all.sh
 
@@ -260,6 +262,8 @@ EOF
 fi
 cat >> tmp/$PRODUCT-$VERSION/flash-all.bat << EOF
 fastboot -w --skip-reboot update image-$PRODUCT-$VERSION.zip
+fastboot reboot-bootloader
+ping -n $SLEEPDURATION 127.0.0.1 >nul
 
 echo Press any key to exit...
 pause >nul
