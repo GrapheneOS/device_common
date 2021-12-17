dabcac982d1d8f0a239907aa51f2a693ce201f0f
flawedworld <flawedworld@flawed.world>
2021-12-17 22:10:43 +0000

    Add support for erasing msadp

--- generate-factory-images-common.sh
+++ generate-factory-images-common.sh
@@ -189,6 +189,13 @@ fastboot erase apdp_a
 fastboot erase apdp_b
 EOF
 fi
+if test "$ERASE_MSADP" = "true"
+then
+cat >> tmp/$PRODUCT-$VERSION/flash-all.sh << EOF
+fastboot erase msadp_a
+fastboot erase msadp_b
+EOF
+fi
 cat >> tmp/$PRODUCT-$VERSION/flash-all.sh << EOF
 fastboot -w --skip-reboot update image-$PRODUCT-$VERSION.zip
 fastboot reboot-bootloader
@@ -284,6 +291,13 @@ then
 cat >> tmp/$PRODUCT-$VERSION/flash-all.bat << EOF
 fastboot erase apdp_a
 fastboot erase apdp_b
+EOF
+fi
+if test "$ERASE_MSADP" = "true"
+then
+cat >> tmp/$PRODUCT-$VERSION/flash-all.bat << EOF
+fastboot erase msadp_a
+fastboot erase msadp_b
 EOF
 fi
 cat >> tmp/$PRODUCT-$VERSION/flash-all.bat << EOF
