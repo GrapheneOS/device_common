efcd42f2784ed134a11086fa61f097abf98f9deb
flawedworld <flawedworld@flawed.world>
2021-12-17 22:35:42 +0000

    Add support for disabling FIPS

--- generate-factory-images-common.sh
+++ generate-factory-images-common.sh
@@ -196,6 +196,12 @@ fastboot erase msadp_a
 fastboot erase msadp_b
 EOF
 fi
+if test "$DISABLE_FIPS" = "true"
+then
+cat >> tmp/$PRODUCT-$VERSION/flash-all.sh << EOF
+fastboot erase fips
+EOF
+fi
 cat >> tmp/$PRODUCT-$VERSION/flash-all.sh << EOF
 fastboot -w --skip-reboot update image-$PRODUCT-$VERSION.zip
 fastboot reboot-bootloader
@@ -298,6 +304,12 @@ then
 cat >> tmp/$PRODUCT-$VERSION/flash-all.bat << EOF
 fastboot erase msadp_a
 fastboot erase msadp_b
+EOF
+fi
+if test "$DISABLE_FIPS" = "true"
+then
+cat >> tmp/$PRODUCT-$VERSION/flash-all.bat << EOF
+fastboot erase fips
 EOF
 fi
 cat >> tmp/$PRODUCT-$VERSION/flash-all.bat << EOF
