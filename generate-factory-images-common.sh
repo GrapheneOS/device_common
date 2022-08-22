6b2e211b2dceee1e3b64a067eedec09bc0caa0a5
flawedworld <flawedworld@flawed.world>
2022-08-23 00:14:46 +0100

    Add support for erasing DPM

--- generate-factory-images-common.sh
+++ generate-factory-images-common.sh
@@ -233,6 +233,13 @@ cat >> tmp/$PRODUCT-$VERSION/flash-all.sh << EOF
 fastboot erase fips
 EOF
 fi
+if test "$DISABLE_DPM" = "true"
+then
+cat >> tmp/$PRODUCT-$VERSION/flash-all.sh << EOF
+fastboot erase dpm_a
+fastboot erase dpm_b
+EOF
+fi
 cat >> tmp/$PRODUCT-$VERSION/flash-all.sh << EOF
 fastboot snapshot-update cancel
 fastboot -w --skip-reboot update image-$PRODUCT-$VERSION.zip
@@ -390,6 +397,13 @@ if test "$DISABLE_FIPS" = "true"
 then
 cat >> tmp/$PRODUCT-$VERSION/flash-all.bat << EOF
 fastboot erase fips
+EOF
+fi
+if test "$DISABLE_DPM" = "true"
+then
+cat >> tmp/$PRODUCT-$VERSION/flash-all.bat << EOF
+fastboot erase dpm_a
+fastboot erase dpm_b
 EOF
 fi
 cat >> tmp/$PRODUCT-$VERSION/flash-all.bat << EOF
