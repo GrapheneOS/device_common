c2707fecfa081b74b33134da29576da567050d6f
quh4gko8 <88831734+quh4gko8@users.noreply.github.com>
2022-08-24 07:30:04 +0000

    flash bootloader on other slot twice

--- generate-factory-images-common.sh
+++ generate-factory-images-common.sh
@@ -164,7 +164,12 @@ fastboot flash xloader xloader-$DEVICE-$XLOADER.img
 EOF
 fi
 cat >> tmp/$PRODUCT-$VERSION/flash-all.sh << EOF
-fastboot flash bootloader bootloader-$DEVICE-$BOOTLOADER.img
+fastboot flash --slot=other bootloader bootloader-$DEVICE-$BOOTLOADER.img
+fastboot --set-active=other
+fastboot reboot-bootloader
+sleep $SLEEPDURATION
+fastboot flash --slot=other bootloader bootloader-$DEVICE-$BOOTLOADER.img
+fastboot --set-active=other
 EOF
 if test "$TWINBOOTLOADERS" = "true"
 then
@@ -297,7 +302,12 @@ fastboot flash xloader xloader-$DEVICE-$XLOADER.img
 EOF
 fi
 cat >> tmp/$PRODUCT-$VERSION/flash-all.bat << EOF
-fastboot flash bootloader bootloader-$DEVICE-$BOOTLOADER.img
+fastboot flash --slot=other bootloader bootloader-$DEVICE-$BOOTLOADER.img
+fastboot --set-active=other
+fastboot reboot-bootloader
+ping -n $SLEEPDURATION 127.0.0.1 >nul
+fastboot flash --slot=other bootloader bootloader-$DEVICE-$BOOTLOADER.img
+fastboot --set-active=other
 EOF
 if test "$TWINBOOTLOADERS" = "true"
 then
